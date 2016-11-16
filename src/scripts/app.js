7fb8bfe80ad4385b48843cb1c2b3cd2976f4c232
jonathandavidblack <jonnybl915@gmail.com>
2016-11-16 12:44:20 -0500

    added toDoModel and toDoCollection to models.js file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,10 @@
 const ReactDOM = require('react-dom');
 const React = require('react');
 const Backbone = require('backbone');
+const ToDoModel = 
 
 
 
-let ToDoModel = Backbone.Model.extend({
-
-
-});
 
 
 const HomeView = React.createClass({
---
+++ src/scripts/models.js
@@ -0,0 +1,17 @@
+const Backbone = require('backbone');
+
+const toDoModel = Backbone.Model.extend({
+   url: '/api/todos/',
+   idAttribute: "_id",
+})
+
+const toDoCollection = Backbone.Collection.extend({
+   model: toDoModel,
+   url: "/api/todos"
+})
+
+
+module.exports = {
+   toDoModel,
+   toDoCollection
+}
